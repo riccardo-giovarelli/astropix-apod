6bed1c519dca6b88bbb4219678c2c97e790b602e
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2020-08-29 04:24:32 +0200

    feat: base for widget

--- astronomy-picture-of-the-day.php
+++ astronomy-picture-of-the-day.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              https://www.riccardogiovarelli.it/
  * @since             1.0.0
  * @package           Astronomy_Picture_Of_The_Day
@@ -15,68 +8,83 @@
  * @wordpress-plugin
  * Plugin Name:       Astronomy Picture of the Day
  * Plugin URI:        https://www.riccardogiovarelli.it/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Riccardo Giovarelli
  * Author URI:        https://www.riccardogiovarelli.it/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           GPL-3.0
  * Text Domain:       astronomy-picture-of-the-day
  * Domain Path:       /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+ // The widget class
+class Astronomy_Picture_Of_The_Day extends WP_Widget {
+
+	// Main constructor
+	public function __construct() {
+		parent::__construct(
+			'astronomy_picture_of_the_day',
+			__( 'Astronomy Picture Of The Day', 'text_domain' ),
+			array(
+				'customize_selective_refresh' => true,
+			)
+		);
+	}
+
+	// The widget form (for the backend )
+	public function form( $instance ) {
+
+		// Set widget defaults
+		$defaults = array(
+			'rss_url'    => '',
+		);
+		
+		// Parse current settings with defaults
+		extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
+
+		<?php // RSS url ?>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'rss_url' ) ); ?>"><?php _e( 'RSS url', 'text_domain' ); ?></label>
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'rss_url' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'rss_url' ) ); ?>" type="text" value="<?php echo esc_attr( $rssUrl ); ?>" />
+		</p>
+
+	<?php }
+
+	// Update widget settings
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['rss_url']    = isset( $new_instance['rss_url'] ) ? wp_strip_all_tags( $new_instance['rss_url'] ) : '';
+		return $instance;
+	}
+
+	// Display the widget
+	public function widget( $args, $instance ) {
+
+		extract( $args );
+
+		// Check the widget options
+		$rssUrl    = isset( $instance['rss_url'] ) ? apply_filters( 'widget_rss_url', $instance['rss_url'] ) : '';
+
+		// WordPress core before_widget hook (always include )
+		echo $before_widget;
+
+		// Display the widget
+		echo '<div class="widget-text wp_widget_plugin_box">';
+
+		// Display RSS url if defined
+		if ( $rssUrl ) {
+			echo $before_title . $rssUrl . $after_title;
+		}
+
+		echo '</div>';
+
+		// WordPress core after_widget hook (always include )
+		echo $after_widget;
+	}
+
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'ASTRONOMY_PICTURE_OF_THE_DAY_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-astronomy-picture-of-the-day-activator.php
- */
-function activate_astronomy_picture_of_the_day() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-astronomy-picture-of-the-day-activator.php';
-	Astronomy_Picture_Of_The_Day_Activator::activate();
+// Register the widget
+function my_register_custom_widget() {
+	register_widget( 'Astronomy_Picture_Of_The_Day' );
 }
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-astronomy-picture-of-the-day-deactivator.php
- */
-function deactivate_astronomy_picture_of_the_day() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-astronomy-picture-of-the-day-deactivator.php';
-	Astronomy_Picture_Of_The_Day_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_astronomy_picture_of_the_day' );
-register_deactivation_hook( __FILE__, 'deactivate_astronomy_picture_of_the_day' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-astronomy-picture-of-the-day.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_astronomy_picture_of_the_day() {
-
-	$plugin = new Astronomy_Picture_Of_The_Day();
-	$plugin->run();
-
-}
-run_astronomy_picture_of_the_day();
+add_action( 'widgets_init', 'my_register_custom_widget' );
