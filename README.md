10e18e1d738cf3795dfbcf60e723e4d6bce9ee72
Riccardo Giovarelli <riccardo.giovarelli@gmail.com>
2020-09-01 00:50:07 +0200

    feat: video support

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wordpress plugin: Astronomy picture of the day
+
+## Authors
+
+Riccardo Giovarelli - riccardo.giovarelli@gmail.com
+
+## Main requirements
+
+* WordPress 5.5+
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the LICENSE.md file for details
--- resources/lib.php
+++ resources/lib.php
@@ -11,6 +11,7 @@ function get_rss_data($rss_url, $base_url) {
     // Regex
     $page_regex = '/href="(?<page_url>[^\"]+)"/i';
     $img_regex = '/img\s*src="(?<img_url>[^\"]+)"/i';
+    $video_regex = '/<iframe\s*width="[0-9]+"\s*height="[0-9]+"\s*src="(?<video_url>[^"]+)"/i';
     $alt_text = '/alt="(?<alt_text>[^\"]+)"/i';
 
     // Get rss content
@@ -26,8 +27,13 @@ function get_rss_data($rss_url, $base_url) {
     if ($page_content === false) return false;
 
     // Get image url
-    if (preg_match_all($img_regex, $page_content, $matched_image_url) === false) return false;
-    $result['image'] = $base_url . reset($matched_image_url['img_url']);
+    if (preg_match_all($img_regex, $page_content, $matched_image_url) > 0) {
+        $result['image'] = $base_url . reset($matched_image_url['img_url']);
+    } else if (preg_match_all($video_regex, $page_content, $matched_video_url) > 0) {
+        $result['video'] = reset($matched_video_url['video_url']);
+    } else {
+        return false;
+    }
 
     // Get image alt text
     $result['alt_text'] = preg_match_all($alt_text, $rss_content, $matched_alt_text) === false ? 
@@ -40,8 +46,23 @@ function get_rss_data($rss_url, $base_url) {
 
 function get_apod_html($rss_data) {
     $html = '';
-    $html .= '<img src="' . $rss_data['image'] . '" alt="' . $rss_data['alt_text'] . '">';
-    $html .= '<div>' . $rss_data['alt_text'] . '</div>';
+    $html .= '<div class="apod_container">';
+        switch (true) {
+            case(isset($rss_data['image'])):
+                $html .= '<img src="' . $rss_data['image'] . '" alt="' . $rss_data['alt_text'] . '">';
+                break;
+            case(isset($rss_data['video'])):
+                $html .= '<iframe src="' . $rss_data['video'] . '" height="315" title="' . $rss_data['alt_text'] . '">';
+                $html .= '</iframe>';
+                break;
+            default:
+                $html .= '<div>' . $rss_data['alt_text'] . '</div>';
+                break;
+        }
+        $html .= '<div>';
+            $html .= empty($rss_data['alt_text']) ? $rss_data['alt_text'] : __( 'no-content-from-rss', 'astropix-apod' );
+        $html .= '</div>';
+    $html .= '</div>';
 
     return $html;
 }
